62ef4c9d022a952aeea34a403c4373767b7e6030
yurihventura <yuri_ven@hotmail.com>
2024-07-20 20:07:35 -0300

    Calculadora itau

---
+++ calculadora.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+
+{
+
+    static void Main()
+
+    {
+
+        int a = 14, b = 8;
+
+        Console.WriteLine($"{a} - {b} = {a - b}");
+
+
+
+        int c = 5, d = 6;
+
+        Console.WriteLine($"{c} * {d} = {c * d}");
+
+
+
+        long e = 2147483647, f = 2;
+
+        Console.WriteLine($"{e} + {f} = {e + f}");
+
+
+
+        int g = 18, h = 3;
+
+        Console.WriteLine($"{g} / {h} = {g / h}");
+
+    }
+
+}
